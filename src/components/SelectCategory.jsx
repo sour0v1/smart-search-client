bc11c0e9557dd94cd1e21d7d587183585c103563
Sourov <sourovdas730@gmail.com>
2024-08-17 21:22:33 +0600

    add : category filter

---
+++ src/components/SelectCategory.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FaCheckSquare, FaRegSquare } from 'react-icons/fa';
+
+const SelectCategory = ({handleSelectCategory, categorys, categoryName}) => {
+    return (
+        <div className='flex justify-center items-center gap-2 hover:bg-gray-100 pl-2 pr-6 py-2 rounded-lg'>
+            <button onClick={() => handleSelectCategory(`${categoryName}`)} className='text-xl'>
+                {categorys?.includes(`${categoryName}`) ? <FaCheckSquare /> : <FaRegSquare />}
+            </button>
+            <span>{categoryName}</span>
+        </div>
+    );
+};
+
+export default SelectCategory;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { FaCheckSquare, FaRegSquare } from 'react-icons/fa';
 import SelectBrand from '../../components/SelectBrand';
+import SelectCategory from '../../components/SelectCategory';
 
 const Home = () => {
     const [clickBrand, setClickBrand] = useState(false);
     const [brands, setBrands] = useState([]);
+    const [categorys, setCategorys] = useState([]);
     console.log(brands);
+    console.log(categorys)
 
+    // handle brand
     const handleSelectBrand = (brand) => {
         console.log(brand)
         setBrands((prevSelectedBrands) => {
@@ -16,37 +20,44 @@ const Home = () => {
         })
     }
 
+    // handleCategorySelect
+    const handleSelectCategory = (category) => {
+        console.log(category)
+        setCategorys((prevSelectedcategorys) => {
+            return prevSelectedcategorys?.includes(category) ?
+                prevSelectedcategorys?.filter((c) => c !== category) : //if already selected the remove it
+                [...prevSelectedcategorys, category] // if not selected the add it
+        })
+    }
+
     return (
-        <div className='max-w-6xl mx-auto space-y-9 mt-6 px-4 lg:px-0'>
-            <input className='w-1/3 bg-gray-100 py-2 px-3 rounded-full outline-none' type="text" />
-            <div className='flex justify-between items-center gap-6'>
-                <div className='flex justify-center items-center gap-6'>
-                    {/* filter product */}
-                    <div className='relative'>
-                        <button onClick={() => setClickBrand(!clickBrand)} className='border py-2 px-6 rounded-lg'>Filter</button>
-                        <div className={`bg-gray-200 absolute left-0 top-full grid grid-cols-3 min-w-[500%] gap-3 mt-1 p-3 rounded-lg duration-200 ${clickBrand ? 'opacity-100 ' : 'opacity-0 pointer-events-none'} justify-items-start overflow-y-scroll h-[100px]`}>
-                            {/* Filter by brand name */}
-                            <h2 className='font-bold col-span-3 border-b pb-2 border-black w-full'>Brand Name</h2>
-                            <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Apple'}></SelectBrand>
-                            <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Samsung'}></SelectBrand>
-                            <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Vivo'}></SelectBrand>
-                            <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Google'}></SelectBrand>
-                            <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Oneplus'}></SelectBrand>
-                            <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Infinix'}></SelectBrand>
-                            <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Htc'}></SelectBrand>
+        <div className='relative w-full max-w-6xl mx-auto'>
+            <div className='flex justify-start items-center gap-6'>
+                <button onClick={() => setClickBrand(!clickBrand)} className='border py-2 px-6 rounded-lg'>Filter</button>
+                <button className='border py-2 px-6 rounded-lg'>Sort by :</button>
+            </div>
+            <div className={`bg-gray-200 absolute left-0 top-full grid grid-cols-2 lg:grid-cols-3 gap-3 mt-1 p-3 rounded-lg duration-200 ${clickBrand ? 'opacity-100 ' : 'opacity-0 pointer-events-none'} justify-items-start overflow-y-scroll h-[300px] w-full lg:w-fit`}>
+                {/* Filter by brand name */}
+                <h2 className='font-bold border-b pb-2 col-span-2 lg:col-span-3 border-black w-full'>Brand Name</h2>
+                <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Apple'}></SelectBrand>
+                <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Samsung'}></SelectBrand>
+                <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Vivo'}></SelectBrand>
+                <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Google'}></SelectBrand>
+                <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Oneplus'}></SelectBrand>
+                <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Infinix'}></SelectBrand>
+                <SelectBrand handleSelectBrand={handleSelectBrand} brands={brands} brandName={'Htc'}></SelectBrand>
 
-                            {/* Filter by category name */}
-                        </div>
+                {/* Filter by category name */}
+                <h2 className='font-bold border-b pb-2 col-span-2 lg:col-span-3 border-black w-full'>Category Name</h2>
+                <SelectCategory handleSelectCategory={handleSelectCategory} categorys={categorys} categoryName={'Android'}></SelectCategory>
+                <SelectCategory handleSelectCategory={handleSelectCategory} categorys={categorys} categoryName={'iOS'}></SelectCategory>
+                <SelectCategory handleSelectCategory={handleSelectCategory} categorys={categorys} categoryName={'5G'}></SelectCategory>
+                <SelectCategory handleSelectCategory={handleSelectCategory} categorys={categorys} categoryName={'Foldable'}></SelectCategory>
+                <SelectCategory handleSelectCategory={handleSelectCategory} categorys={categorys} categoryName={'Gaming'}></SelectCategory>
 
-                    </div>
-                </div>
-
-                {/* Sort Product */}
-                <div className='border py-2 px-6 rounded-lg'>
-                    Sort By : <span>Price</span>
-                </div>
             </div>
         </div>
+
     );
 };
 
